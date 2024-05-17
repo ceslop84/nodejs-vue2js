26fd546dcd25ab6fafc6b011249baae0ab8255a4
Cesar Rafael Lopes <cesar.lopes@serpro.gov.br>
2024-05-17 09:46:58 -0300

    carga

--- main.js
+++ main.js
@@ -2,8 +2,26 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        image: './img/socks.jpg',
+        brand: "Master",
+        image: './img/sock_green.jpg',
         link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
-        inventory: 0
+        inStock: false,
+        inventory: 0,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [{variantId: 2234, variantColor: "green", variantImage: "./img/sock_green.jpg"}, {variantid: 2235, variantColor: "blue", variantImage: "./img/sock_blue.jpg"}],
+        cart: 0
+    },
+    methods: {
+        addToCart: function (){
+            this.cart += 1
+        },
+        updateImage: function (variantImage){
+            this.image = variantImage
+        },
+    },
+    computed:{
+        title(){
+            return this.brand + ' ' + this.product                    
+        }
     }
 })
